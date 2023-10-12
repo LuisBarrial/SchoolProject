1e57137219ea3470e6859845bcccc9c899974470
LuisBarrial <angeloxz.com@gmail.com>
2023-10-12 14:43:18 -0500

    configurando vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  pages: ['src/pages/Index.jsx'],
+  pages: ['src/pages/Indexs.jsx'],
 
 })
