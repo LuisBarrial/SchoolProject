4a4ea78950f1bfe369989caa2b0f62a3e1be6e0d
LuisBarrial <angeloxz.com@gmail.com>
2023-10-12 15:58:17 -0500

    add page questions

--- src/pages/Preguntas.jsx
+++ src/pages/Preguntas.jsx
@@ -1,11 +1,39 @@
 const Preguntas = () => {
-    return(
-        <>
-            <div>
-                <h1>Preguntas</h1>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <h1>Preguntas</h1>
+        <section id="preguntas">
+          <h2>Frecuentes</h2>
+          <p>Aquí hay algunas preguntas que pueden surgir:</p>
+          <ul>
+            <li>
+              <h3>¿Cómo funciona el sitio?</h3>
+              <p>
+                El sitio funciona mediante un sistema de preguntas y respuestas.
+                Los usuarios pueden publicar preguntas y responder a las
+                preguntas de otros usuarios.
+              </p>
+            </li>
+            <li>
+              <h3>¿Cuál es el propósito del sitio?</h3>
+              <p>
+                El propósito del sitio es proporcionar un foro para que los
+                usuarios puedan compartir información y aprender unos de otros.
+              </p>
+            </li>
+            <li>
+              <h3>¿Quiénes son los creadores del sitio?</h3>
+              <p>
+                El sitio fue creado por un equipo de desarrolladores de
+                software.
+              </p>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </>
+  );
+};
 
 export default Preguntas;
